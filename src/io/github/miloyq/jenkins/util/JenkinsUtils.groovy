2a73af3ee40c50e5ec87bcb954bb0f57948c4018
miloyq <13917665872wyq@gmail.com>
2025-11-14 00:56:20 +0800

    feat(util): add JenkinsUtils for pipeline context resolution

---
+++ src/io/github/miloyq/jenkins/util/JenkinsUtils.groovy
@@ -0,0 +1,15 @@
+package io.github.miloyq.jenkins.util
+
+class JenkinsUtils implements Serializable {
+    static String resolveScriptName(script, String defaultName = 'Pipeline') {
+        try {
+            def clazz = script?.getClass()
+            def name = clazz?.simpleName
+            if (name && !name.endsWith('WorkflowScript') && !name.contains('CpsScript')) {
+                return name
+            }
+        } catch (ignored) {
+        }
+        return script?.env?.JOB_NAME ?: defaultName
+    }
+}

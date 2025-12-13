df89f3b8699e3983e2a24a386aafb42cfddb4207
miloyq <13917665872wyq@gmail.com>
2025-12-13 22:08:05 +0800

    feat(config): add mergeAll method to ConfigMerger

--- src/io/github/miloyq/jsl/config/ConfigMerger.groovy
+++ src/io/github/miloyq/jsl/config/ConfigMerger.groovy
@@ -3,7 +3,15 @@ package io.github.miloyq.jsl.config
 import io.github.miloyq.jsl.config.strategy.DeepMergeStrategy
 
 class ConfigMerger {
-    static Object merge(Object base, Object override, MergeStrategy strategy = new DeepMergeStrategy()) {
+    static Object merge(Object base, Object override,
+                        MergeStrategy strategy = new DeepMergeStrategy()) {
         return strategy.merge(base, override)
+    }
+
+    static Object mergeAll(Object base, List overrides,
+                           MergeStrategy strategy = new DeepMergeStrategy()) {
+        overrides.inject(base) { acc, o ->
+            strategy.merge(acc, o)
+        }
     }
 }
